9e499be74df7a03c4deb3018bb8c6e7b00ac44f5
Equoda <inconsistent_code@tutanota.com>
2026-05-24 18:03:56 +0200

    feature: Add GPT models

--- tests/vllm-send-prompt.sh
+++ tests/vllm-send-prompt.sh
@@ -3,8 +3,9 @@
 SCRIPT_DIR=$(dirname "$(readlink -f """${0}""")")
 
 PROMPT=${1:-Make a joke, in one sentence}
-MODEL_NAME=${2:-Qwen/Qwen2.5-3B-Instruct}
-ADDRESS=${3:-localhost:8000}
+MAX_TOKENS=${2:-100}
+MODEL_NAME=${3:-Qwen/Qwen2.5-3B-Instruct}
+ADDRESS=${4:-localhost:8000}
 
 source "${SCRIPT_DIR}/../../secrets/vllm/.env"
 
@@ -16,6 +17,6 @@ curl -v "http://${ADDRESS}/v1/chat/completions" \
 	  \"messages\": [
 	    {\"role\": \"user\", \"content\": \"${PROMPT}\"}
 	  ],
-	  \"max_tokens\": 100
+	  \"max_tokens\": ${MAX_TOKENS}
 	}" \
 	| jq '.'
